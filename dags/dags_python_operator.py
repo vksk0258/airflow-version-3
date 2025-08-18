caed2fd80ffff8f1ffb156658df7d3216642884b
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2025-08-19 08:51:52 +0900

    tags 추가

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -14,7 +14,8 @@ with DAG(
     dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=False,
+    tags=["길택"]
 ) as dag:
     def select_fruit():
         fruit = ['APPLE','BANANA','ORANGE','AVOCADO']
