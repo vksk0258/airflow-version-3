24ed6ba9050ad9523e00d1ab48c1e8582d07fa63
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2025-08-19 09:32:04 +0900

    dags_python_import_func 추가

---
+++ dags/dags_python_import_func.py
@@ -0,0 +1,23 @@
+import pendulum
+from common.common_func import get_sftp
+
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow.operators.python import PythonOperator
+#from airflow import DAG
+
+with DAG(
+    dag_id="dags_python_import_func",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+    tags=["길택"]
+) as dag:
+
+    task_get_sftp = PythonOperator(
+        task_id='task_get_sftp',
+        python_callable=get_sftp
+    )
