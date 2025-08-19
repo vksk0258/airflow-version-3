ca6a0ec9ffb59be3ef93adfbe2542e2a9d3bb273
NB-4002245\DKSYSTEMS <vksk0258@gmail.com>
2025-08-19 10:49:09 +0900

    dags_python_task_decorator 생성

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,21 @@
+import pendulum
+
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG, task
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow import DAG
+#from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False,
+) as dag:
+    
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1 = print_context('task_decorator 실행')
